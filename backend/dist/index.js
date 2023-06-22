514099418a3abaea30dd3e5d7b0dbd0a5e5f17d2
shima_dev <104167280+shimarrudz@users.noreply.github.com>
2023-06-21 21:34:00 -0300

    fixing: removing node modules and dist

--- backend/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var BinaryConverter_1 = require("./BinaryConverter");
-var binaryNumber = "1101";
-var converter = new BinaryConverter_1.default();
-var decimalNumber = converter.convertToDecimal(binaryNumber);
-console.log("O n\u00FAmero bin\u00E1rio ".concat(binaryNumber, " \u00E9 igual a ").concat(decimalNumber, " em decimal."));
